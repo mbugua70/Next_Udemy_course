9d0e9474b2ca990ea5b8dd78fbdff9e2af910187
john mbugua <johnymbugua1997@gmail.com>
2024-07-07 19:29:50 +0300

    food app nextjs setup

--- food_app/app/layout.js
+++ food_app/app/layout.js
@@ -1,6 +1,6 @@
-import MainHeaderBackground from "@/components/main-header-background";
+import MainHeaderBackground from "@/components/mainHeader/main-header-background";
 import "./globals.css";
-import MainHeader from "@/components/main-header";
+import MainHeader from "@/components/mainHeader/main-header";
 
 export const metadata = {
   title: "NextLevel Food",
--- food_app/components/mainHeader/main-header.js
+++ food_app/components/mainHeader/main-header.js
@@ -1,10 +1,14 @@
+"use client";
 import Link from "next/link";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 import LogoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 
 export default function MainHeader() {
+  const path = usePathname();
+  console.log(path);
   return (
     <header className={classes.header}>
       <Link className={classes.logo} href="/">
@@ -14,10 +18,20 @@ export default function MainHeader() {
       <nav className={classes.nav}>
         <ul>
           <li>
-            <Link href="/meals">Browse Meals</Link>
+            <Link
+              href="/meals"
+              className={path.startsWith("/meals") ? classes.active : undefined}
+            >
+              Browse Meals
+            </Link>
           </li>
           <li>
-            <Link href="/community">Foodies Community</Link>
+            <Link
+              href="/community"
+              className={path === "community" ? classes.active : undefined}
+            >
+              Foodies Community
+            </Link>
           </li>
         </ul>
       </nav>
