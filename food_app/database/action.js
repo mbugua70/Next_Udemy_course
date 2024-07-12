1425104a8c70acaeb1473e314fd4417c888ae806
john mbugua <johnymbugua1997@gmail.com>
2024-07-12 23:39:45 +0300

    Form submission

---
+++ food_app/database/action.js
@@ -0,0 +1,16 @@
+"use server";
+
+import { saveMeal } from "./meals";
+
+export default async function handleShare(formData) {
+  const meal = {
+    title: formData.get("title"),
+    summary: formData.get("summary"),
+    instructions: formData.get("instructions"),
+    image: formData.get("image"),
+    creator: formData.get("name"),
+    creator_email: formData.get("email"),
+  };
+
+  await saveMeal(meal);
+}
--- food_app/database/meals.js
+++ food_app/database/meals.js
@@ -1,4 +1,7 @@
+import fs from "node:fs";
 import sql from "better-sqlite3";
+import slugify from "slugify";
+import xss from "xss";
 
 const db = sql("meals.db");
 
@@ -11,4 +14,39 @@ export async function getMeals() {
 
 export async function getMeal(slug) {
   return db.prepare("SELECT * FROM meals WHERE slug = ?").get(slug);
+}
+
+export async function saveMeal(meal) {
+  meal.slug = slugify(meal.title, { lower: true });
+  meal.instructions = xss(meal.instructions);
+
+  const extension = meal.image.name.split(".").pop();
+  const filename = `${meal.slug}.${extension}`;
+
+  const stream = fs.createWriteStream(`public/images/${filename}`);
+  const bufferImage = await meal.image.arrayBuffer();
+
+  stream.write(Buffer.from(bufferImage), (error) => {
+    if (error) {
+      throw new Error("Failed to save the image");
+    }
+  });
+
+  meal.image = `/images/${filename}`;
+
+  db.prepare(
+    `
+     INSERT INTO meals
+     (title, instructions, summary, creator, creator_email, slug,image)
+     VALUES(
+      @title,
+      @instructions,
+      @summary,
+      @creator,
+      @creator_email,
+      @slug,
+      @image
+     )
+    `
+  ).run(meal);
 }
